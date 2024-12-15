10680b309dc77767221a39ad838bf6f51fbd9f62
Zai <allenzaire@gmail.com>
2024-12-15 02:41:36 -0500

    fixed again

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,7 @@ export default function Alternative() {
         </span>
 
         {/* Countdown section */}
-        <div className="my-8">
+        <div className="my-8 flex justify-center md:w-full">
           <CountdownClock targetDate={nextSessionDate} />
         </div>
       </div>
